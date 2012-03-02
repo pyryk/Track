03812f0684df6817d9f2eaaae02f5aed2d979050
Pyry Kröger <pyry@krogerit.net>
2012-03-02 17:24:14 +0200

    added basic routing

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,34 @@
+var App = Spine.Controller.sub({
+  pages: {},
+  init: function() {
+    this.routes({
+      "!/targets/": function(params) {
+        if (!this.pages.targetList) {
+          log("creating view targetlist");
+          this.pages.targetList = new ListTargets({
+            el: $("#placeholder")
+          });
+        } else {
+          this.pages.targetList.render();
+        }
+      },
+      "!/targets/:id": function(params) {
+        log("targets", params.id);
+      },
+      // default route
+      "*others": function(params) {
+        log("Invalid route ", params.match.input, " - redirecting to default");
+        Spine.Route.navigate("!/targets/");
+      }
+    });
+    
+    Spine.Route.setup();
+  }
+});
+
+//TODO url resolver/generator
+
 jQuery(function($) {
-  return new ListTargets({
-    el: $("#placeholder")
-  });
+  var app = new App();
+  //Spine.Route.navigate("/targets/11");
 });
