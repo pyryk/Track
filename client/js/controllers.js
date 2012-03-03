9093b8788ab5fc874536bfbfcce95ecbec7ebb6c
Pyry Kröger <pyry@krogerit.net>
2012-03-03 12:30:21 +0200

    added target details view

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -15,6 +15,9 @@ var TargetsList = BaseController.sub({
   elements: {
     ".targets": "targets"  
   },
+  events: {
+    "click #target-list li": "clicked"
+  },
   getData: function() {
     return {items: Target.all()};
   },
@@ -26,4 +29,36 @@ var TargetsList = BaseController.sub({
   addOne: function(task){
     this.render();
   },
+  clicked: function(e) {
+    var id = $(e.target).attr('data-id');
+    if (id) {
+      Spine.Route.navigate(App.getRoute(Target.find(id)));
+    }
+  }
+});
+
+var TargetDetails = BaseController.sub({
+  events: {
+    "click .save-answer": "saveAnswer"
+  },
+  init: function() {
+    this.rawTemplate = this.template || $("#template-TargetDetails");
+    this.template = Handlebars.compile(this.rawTemplate.html());
+  },
+  getData: function() {
+    var target, error;
+    try {
+      target = Target.find(this.id);
+      log("Rendering ", target);
+    } catch (e) {
+      error = e;
+      log(e);
+    }
+    
+    return {target: target, error: error};
+  },
+  saveAnswer: function(e) {
+    var fields = $(e.target).siblings('.target-answer').children('input');
+    log("Saving answer", fields);
+  }
 });
