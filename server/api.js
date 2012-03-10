adb9199fecc7d63346f0d6c29d7b25c764d0dd4a
Mikko Koski <mikkokos@gmail.com>
2012-03-10 13:52:23 +0200

    Error handling

--- server/api.js
+++ server/api.js
@@ -10,18 +10,27 @@ var API = {
     getTargets: function(req, res, next) {
         Mongo.findAllTargets().then(function(data) {
             res.send(200, {targets: data});
+            return next();
+        }, function(error) {
+            return next(error);
         });
     },
 
     getTarget: function(req, res, next) {
         Mongo.findTargetById(req.params.id).then(function(data) {
             res.send(200, {target: data});
+            return next();
+        }, function(error) {
+            return next(error);
         });
     },
 
     postTarget: function(req, res, next) {
         Mongo.createTarget(req.params.name).then(function() {
             res.send(201, "");
+            return next();
+        }, function(error) {
+            return next(error);
         });
     }
 }
--- server/spec/api.spec.js
+++ server/spec/api.spec.js
@@ -2,18 +2,27 @@ var Mongo = require('../mongo');
 var API = require('../api');
 
 describe('API', function() {
-    var req = {}, res = {}, next = {};
+    var req = {}, res = {}, next;
 
     var spyOnPromise = function(Klass, method) {
         var spy = spyOn(Klass, method);
 
-        return {andCallThenWith: function(returnValue) {
-            spy.andReturn({
-                then: function(callback) {
-                    callback(returnValue);
-                }
-            })
-        }};
+        return {
+            andCallSuccess: function(returnValue) {
+                spy.andReturn({
+                    then: function(callback) {
+                        callback(returnValue);
+                    }
+                })
+            },
+            andCallError: function(errorValue) {
+                spy.andReturn({
+                    then: function(callback, error) {
+                        error(errorValue);
+                    }
+                })
+            }
+        };
     };
 
     var expectStatus = function(res, status) {
@@ -44,25 +53,37 @@ describe('API', function() {
         return expect(null);
     };
 
+    var itShouldCallNextWithError = function(apiMethod, mongoMethod) {
+        it('should call next with error', function() {
+            var error = {message: "An error occured"};
+            spyOnPromise(Mongo, mongoMethod).andCallError(error);
+
+            API[apiMethod](req, res, next);
+
+            expect(next).toHaveBeenCalledWith(error);
+        });
+    };
+
     beforeEach(function() {
 
         // Request
         req.params = {};
 
         // Response
-        res.send = jasmine.createSpy();
+        res.send = jasmine.createSpy('res.send');
+
+        // Next
+        next = jasmine.createSpy('next');
     });
 
     describe('getTargets', function() {
 
-        beforeEach(function() {
-            spyOnPromise(Mongo, 'findAllTargets').andCallThenWith([
+        it('should return list of targets', function() {
+            spyOnPromise(Mongo, 'findAllTargets').andCallSuccess([
                 {name: "T-Talon ruokajono", _id: "accab1234"},
                 {name: "Putous", _id: "accab12345"}
             ]);
-        });
 
-        it('should return list of targets', function() {
             API.getTargets(req, res, next);
 
             expectBody(res).toEqual({
@@ -71,20 +92,18 @@ describe('API', function() {
                     {name: "Putous", _id: "accab12345"}
                 ]
             });
-
             expectStatus(res).toEqual(200);
         });
+
+        itShouldCallNextWithError('getTargets', 'findAllTargets');
     });
 
     describe('getTarget', function() {
 
-        beforeEach(function() {
-            spyOnPromise(Mongo, 'findTargetById').andCallThenWith(
+        it('should return details of a target', function() {
+            spyOnPromise(Mongo, 'findTargetById').andCallSuccess(
                 {name: "T-Talon ruokajono", _id: "accab1234"}
             );
-        });
-
-        it('should return details of a target', function() {
             req.params.id = 'accab1234';
 
             API.getTarget(req, res, next);
@@ -92,26 +111,29 @@ describe('API', function() {
             expectBody(res).toEqual({
                 target: {name: "T-Talon ruokajono", _id: "accab1234"}
             });
-
             expectStatus(res).toEqual(200);
         });
+
+        itShouldCallNextWithError('getTarget', 'findTargetById');
     });
 
     describe('postTarget', function() {
 
-        beforeEach(function() {
-            spyOnPromise(Mongo, 'createTarget').andCallThenWith();
-        });
-
         it('should return details of a target', function() {
+            spyOnPromise(Mongo, 'createTarget').andCallSuccess();
             req.params.name = 'New tracking target';
 
             API.postTarget(req, res, next);
 
             expect(Mongo.createTarget).toHaveBeenCalledWith('New tracking target');
-
             expectStatus(res).toEqual(201);
         });
+
+        itShouldCallNextWithError('postTarget', 'createTarget');
+    });
+
+    afterEach(function() {
+        expect(next).toHaveBeenCalled();
     });
 
 
