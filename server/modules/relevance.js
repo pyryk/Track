48d133b5cfc5e5d060a3f843873e07e01c0a7205
Mikko Koski <mikkokos@gmail.com>
2012-03-22 15:42:08 +0200

    Foundation for multiple Relevance calculation strategies

--- server/modules/relevance.js
+++ server/modules/relevance.js
@@ -1,32 +1,109 @@
 
 var Relevance = function() {
     this.maxScore = 10;
-    this.overallPopularityWeight = 1;
+    this.strategies = [OverallPopularity];
 };
 
 Relevance.prototype.calculate = function(targets) {
 
-    var maxResults = 0;
-    var maxResultsSqrt = 0;
+    var strategies = this.strategies;
+    var maxScore = this.maxScore;
+
+    strategies.forEach(function(strategy) {
+        if(strategy.analyzeTargetsStarted) {
+            strategy.analyzeTargetsStarted(targets);
+        }
+    });
 
     // Analyze
     targets.forEach(function(target) {
-        maxResults = Math.max(target.results ? target.results.length : 0, maxResults);
+        strategies.forEach(function(strategy) {
+            if(strategy.analyzeTarget) {
+                strategy.analyzeTarget(target, targets);
+            }
+        });
+
+        var results = target.results || [];
+
+        strategies.forEach(function(strategy) {
+            if(strategy.analyzeResultsStarted) {
+                strategy.analyzeResultsStarted(results, target, targets);
+            }
+        });
+
+        results.forEach(function(result) {
+            strategies.forEach(function(strategy) {
+                if(strategy.analyzeResult) {
+                    strategy.analyzeResult(result, results, target, targets);
+                }
+            });
+        });
+
+        strategies.forEach(function(strategy) {
+            if(strategy.analyzeResultsFinished) {
+                strategy.analyzeResultsFinished(results, target, targets);
+            }
+        });
     });
 
-    maxResultsSqrt = Math.sqrt(maxResults);
+    strategies.forEach(function(strategy) {
+        if(strategy.analyzeTargetsFinished) {
+            strategy.analyzeTargetsFinished(targets);
+        }
+    });
+
+    strategies.forEach(function(strategy) {
+        if(strategy.calculateRelevanciesStarted) {
+            strategy.calculateRelevanciesStarted(targets);
+        }
+    });
 
     // Calculate and set
-    var maxScore = this.maxScore;
-    var weight = this.overallPopularityWeight;
     targets.forEach(function(target) {
-        if(maxResultsSqrt === 0) {
+        var totalRelevance = 0;
+        strategies.forEach(function(strategy) {
+            if(strategy.calculateRelevance) {
+                totalRelevance += strategy.calculateRelevance(target, targets) * maxScore;
+            }
+        });
+
+        target.relevance = totalRelevance;
+    });
+
+    strategies.forEach(function(strategy) {
+        if(strategy.calculateRelevanciesFinished) {
+            strategy.calculateRelevanciesFinished(targets);
+        }
+    });
+};
+
+var OverallPopularity = {
+    weight: 1,
+
+    analyzeTargetsStarted: function() {
+        this.maxResult = 0;
+        this.maxResultSqrt = 0;
+    },
+    analyzeTargetsFinished: function() {
+        this.maxResultSqrt = Math.sqrt(this.maxResult);
+    },
+    analyzeTarget: function(target) {
+        this.maxResult = Math.max(target.results ? target.results.length : 0, this.maxResult);
+    },
+
+    calculateRelevance: function(target, maxScore) {
+
+        if(this.maxResultSqrt === 0) {
             target.relevance = 0;
             return;
         }
 
-        target.relevance = Math.sqrt(target.results ? target.results.length : 0) / maxResultsSqrt * maxScore * weight;
-    });
+        return Math.sqrt(target.results ? target.results.length : 0) / this.maxResultSqrt * this.weight;
+    }
+}
+
+Relevance.Strategy = {
+    OverallPopularity: OverallPopularity
 }
 
 module.exports = Relevance;
--- server/spec/relevance.spec.js
+++ server/spec/relevance.spec.js
@@ -5,10 +5,12 @@ describe('Relevance', function() {
 
     beforeEach(function() {
         rel.maxScore = 10;
-        rel.overallPopularityWeight = 0.5;
     });
 
     it('should calculate relevancy by overall popularity', function() {
+        Relevance.Strategy.OverallPopularity.weight = 0.5;
+        rel.strategies = [Relevance.Strategy.OverallPopularity];
+
         var targets = [
             {results: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]},
             {results: [1, 2, 3, 4, 5, 6, 7, 8, 9]},
@@ -27,4 +29,47 @@ describe('Relevance', function() {
         expect(targets[4].relevance).toEqual(0);
         expect(targets[5].relevance).toEqual(0);
     });
+
+    it('should implement Strategy pattern with hook functions', function() {
+        // Strategy spy
+        var s = {
+            analyzeTargetsStarted: jasmine.createSpy(),
+            analyzeTargetsFinished: jasmine.createSpy(),
+            analyzeTarget: jasmine.createSpy(),
+
+            analyzeResultsStarted: jasmine.createSpy(),
+            analyzeResultsFinished: jasmine.createSpy(),
+            analyzeResult: jasmine.createSpy(),
+
+            calculateRelevanciesStarted: jasmine.createSpy(),
+            calculateRelevanciesFinished: jasmine.createSpy(),
+            calculateRelevance: jasmine.createSpy()
+        }
+
+        var targets = [
+            {results: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]},
+            {results: [1, 2, 3, 4, 5, 6, 7, 8, 9]},
+            {results: [1, 2, 3, 4]},
+            {results: [1]},
+            {results: []},
+            {}
+        ];
+
+        rel.strategies = [s];
+        rel.calculate(targets);
+
+        expect(s.analyzeTargetsStarted.callCount).toEqual(1);
+        expect(s.analyzeTargetsFinished.callCount).toEqual(1);
+        expect(s.analyzeTarget.callCount).toEqual(6);
+
+        expect(s.analyzeResultsStarted.callCount).toEqual(6);
+        expect(s.analyzeResultsFinished.callCount).toEqual(6);
+        expect(s.analyzeResult.callCount).toEqual(30);
+
+        expect(s.calculateRelevanciesStarted.callCount).toEqual(1);
+        expect(s.calculateRelevanciesFinished.callCount).toEqual(1);
+        expect(s.calculateRelevance.callCount).toEqual(6);
+
+    });
+
 });
