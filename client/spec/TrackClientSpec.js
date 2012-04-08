74758d600e23270dcaae5bae71fbf034558c316b
Pyry Kröger <pyry@krogerit.net>
2012-04-08 19:11:23 +0300

    improved testing a bit

--- client/spec/TrackClientSpec.js
+++ client/spec/TrackClientSpec.js
@@ -1,19 +1,43 @@
-describe("Local targets", function() {
-  it("Targets can be created", function() {
+describe("Targets", function() {
+  beforeEach(function() {
+    console.log('setting the server up');
+    this.server = sinon.fakeServer.create();
+  });
+  
+  afterEach(function() {
+    this.server.restore();
+  });
+  
+  it("should be possible to create targets", function() {
     var target = Target.create({
       name: "Test target", 
-      question: "Enter random number of minutes",
+      question: "Is this good?",
     });
     expect(target.id).toBe(Target.last().id);
   });
   
-  it("Targets are shown in the app", function() {
-    var target = Target.create({
-      name: "Test target", 
-      question: "Enter random number of minutes",
-    });
-    var listItems = $('iframe').contents().find("li[data-id=" + target.id + "]");
-    console.log(target.id);
-    expect(listItems.size()).toEqual(0); // TODO fix
+  it("should be possible to navigate to a target", function() {
+    
+    this.server.respondWith("GET", "/targets",
+      [200, {"Content-Type": "application/json"},
+      '{"targets":[{"name":"Trackin uusi ulkoasu","_id":"4f690001f24705a2020000c2","question":"Mitä tykkäät?","relevance":0}]}'
+      ]);
+    
+    window.track = new App();
+    
+    // loads target list
+    Spine.Route.navigate('#!/');
+    // responds to target list call
+    this.server.respond(); 
+  
+    
+    var target = Target.first();
+    // mock event
+    var e = {target: '<li data-id="' + target.id + '"></li>'};
+    var list = window.track.visiblePage;
+    window.track.visiblePage.clicked(e);
+    
+    expect(window.track.visiblePage).toNotBe(list)
+    
   });
 });
