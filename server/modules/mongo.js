af15fd9f25e2732aefcf065121ca2bfdc5201653
hkorhone <hkorhone@gmail.com>
2012-06-06 12:45:11 +0300

    Reimplementation of delete method.

--- server/modules/mongo.js
+++ server/modules/mongo.js
@@ -4,6 +4,7 @@ var Promise = p.Promise;
 var Fixtures = require('../fixtures/fixtures');
 var DateUtils = require('../modules/now.js');
 var _ = require('underscore');
+var restify = require('restify');
 
 var Mongo = {
 
@@ -201,9 +202,7 @@ var Mongo = {
             Mongo.resolvePromise(error, promise)
         }.bind(this));
 
-
         return promise;
-
 
     },
 
@@ -211,9 +210,16 @@ var Mongo = {
 
         var promise = Promise();
 
-        this.Target.remove({_id: targetId}, function(error) {
-            Mongo.resolvePromise(error, promise)
-        }.bind(this));
+        Mongo.findTargetById(targetId).then(function(data) {
+            if(data == null) {
+                Mongo.resolvePromise(new restify.ResourceNotFoundError("Could not find target with ID " + targetId)
+                    , data, promise);
+            } else {
+                Mongo.deleteTarget(data).then(function error(err) {
+                    Mongo.resolvePromise(err, data, promise);
+                })
+            }
+         });
 
         return promise;
 
