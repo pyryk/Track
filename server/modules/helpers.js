d67eebea37ad9968b28bc641b1b220f495ecdf0b
hkorhone <hkorhone@gmail.com>
2012-07-16 13:05:32 +0300

    Added new addResult functionality. Removed unused helpers.js. Changed API's post result endpoint into /result/:questionId. TODO: tests, readme.

--- server/modules/helpers.js
+++
@@ -1,9 +0,0 @@
-Object.prototype.clone = function() {
-    var newObj = (this instanceof Array) ? [] : {};
-    for (i in this) {
-        if (i == 'clone') continue;
-        if (this[i] && typeof this[i] == "object") {
-            newObj[i] = this[i].clone();
-        } else newObj[i] = this[i]
-    } return newObj;
-};
