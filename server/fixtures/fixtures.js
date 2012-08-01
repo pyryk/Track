22b61cb6e079c9a84b3bf92ecef1405135233950
hkorhone <hkorhone@gmail.com>
2012-08-01 11:44:31 +0300

    Updated fixtures to reflect new customer-target-question-result relations.

--- server/fixtures/fixtures.js
+++ server/fixtures/fixtures.js
@@ -1,48 +1,44 @@
 // Now: Date('2012-03-23T08:03:48.223Z')
 
 module.exports = {
-    targets: [{
-        name: 'T-Talon ruokajono',
-        _id: '12345678901234567890abce',
-        question: 'Oliko paljon jonoa?',
-        results : [
-        { value : 1, timestamp : new Date('2012-03-23T08:03:48.223Z') },
-        { value : 1, timestamp : new Date('2012-03-23T08:04:48.223Z') },
-        { value : 1, timestamp : new Date('2012-03-23T08:05:48.223Z') },
-        { value : 0, timestamp : new Date('2012-03-23T08:10:48.223Z') },
-        { value : 0, timestamp : new Date('2012-03-23T08:50:48.223Z') },
-        { value : 0, timestamp : new Date('2012-03-23T08:51:48.223Z') },
-        { value : 1, timestamp : new Date('2012-03-23T10:03:48.223Z') },
-        { value : 1, timestamp : new Date('2012-03-23T10:13:48.223Z') },
-        { value : 1, timestamp : new Date('2012-03-23T10:14:48.223Z') },
-        { value : 0, timestamp : new Date('2012-03-23T12:01:48.223Z') },
-        { value : 0, timestamp : new Date('2012-03-23T12:03:48.223Z') },
-        { value : 0, timestamp : new Date('2012-03-23T12:05:48.223Z') },
-        { value : 1, timestamp : new Date('2012-03-23T13:33:48.223Z') },
-        { value : 1, timestamp : new Date('2012-03-23T13:43:48.223Z') },
-        { value : 1, timestamp : new Date('2012-03-23T13:50:48.223Z') },
-        { value : 0, timestamp : new Date('2012-03-23T13:51:48.223Z') } ]
-    }, {
-        name: 'Mikä fiilis?',
-        _id: '12345678901234567890abcd',
-        question: 'Millainen fiilis sinulla on tällä hetkellä?',
-        results : [
-        { value : 1, timestamp : new Date('2012-03-23T08:03:48.223Z'), fbUserId: '123456'},
-        { value : 1, timestamp : new Date('2012-03-23T08:02:48.223Z') },
-        { value : 1, timestamp : new Date('2012-03-23T08:02:28.223Z') },
-        { value : 0, timestamp : new Date('2012-03-23T08:01:48.223Z') },
-        { value : 0, timestamp : new Date('2012-03-23T08:01:18.223Z') },
-        { value : 0, timestamp : new Date('2012-03-23T08:00:08.223Z') },
-        { value : 1, timestamp : new Date('2012-03-23T07:59:48.223Z') },
-        { value : 1, timestamp : new Date('2012-03-23T07:59:11.223Z') },
-        { value : 1, timestamp : new Date('2012-03-23T07:53:48.223Z') },
-        { value : 0, timestamp : new Date('2012-03-23T07:43:48.223Z') }]
-    }, {
-        name: 'Putouksen munamiehen läpän taso',
-        _id: '12345678901234567890abcf',
-        question: 'No millasta läpyskää puskee?',
-        results: [1, 2, 3, 4]
-    }],
+
+    customers: [
+        { _id: '12345678901234567890cbcd', name: 'Aalto-yliopisto' },
+        { _id: '12345678901234567890cbce', name: 'McDonald\'s' },
+        { _id: '12345678901234567890cbcf', name: 'Hesburger' }
+    ],
+
+    targets: [
+        { name: 'Matematiikka C1', _id: '12345678901234567890abce', customerId: '12345678901234567890cbcd' },
+        { name: 'Matematiikka C2', _id: '12345678901234567890abcd', customerId: '12345678901234567890cbcd' },
+        { name: 'Matematiikka C3', _id: '12345678901234567890abce', customerId: '12345678901234567890cbcd' }
+    ],
+
+    questions: [
+        { _id: '12345678901234567890bbcd', name: 'Opettaako luennoitsija hyvin?', targetId: '12345678901234567890abcd' },
+        { _id: '12345678901234567890bbce', name: 'Toimivatko kurssin järjestelyt?', targetId: '12345678901234567890abcd' },
+        { _id: '12345678901234567890bbcf', name: 'Onko kurssi haastava?', targetId: '12345678901234567890abcd' },
+        { _id: '12345678901234567890bbcg', name: 'Suosittelisitko kurssia kaverille?', targetId: '12345678901234567890abcd' }
+    ],
+
+    results : [
+        { _id: '12345678901234567890dbcd', value : 1, timestamp : new Date('2012-03-23T08:03:48.223Z'), fbUserId: '123456' },
+        { _id: '12345678901234567890dbce', value : 1, timestamp : new Date('2012-03-23T08:04:48.223Z') },
+        { _id: '12345678901234567890dbcf', value : 1, timestamp : new Date('2012-03-23T08:05:48.223Z') },
+        { _id: '12345678901234567890dbcg', value : 0, timestamp : new Date('2012-03-23T08:10:48.223Z') },
+        { _id: '12345678901234567890dbch', value : 0, timestamp : new Date('2012-03-23T08:50:48.223Z') },
+        { _id: '12345678901234567890dbci', value : 0, timestamp : new Date('2012-03-23T08:51:48.223Z') },
+        { _id: '12345678901234567890dbcj', value : 1, timestamp : new Date('2012-03-23T10:03:48.223Z') },
+        { _id: '12345678901234567890dbck', value : 1, timestamp : new Date('2012-03-23T10:13:48.223Z') },
+        { _id: '12345678901234567890dbcl', value : 1, timestamp : new Date('2012-03-23T10:14:48.223Z') },
+        { _id: '12345678901234567890dbcm', value : 0, timestamp : new Date('2012-03-23T12:01:48.223Z') },
+        { _id: '12345678901234567890dbcn', value : 0, timestamp : new Date('2012-03-23T12:03:48.223Z') },
+        { _id: '12345678901234567890dbco', value : 0, timestamp : new Date('2012-03-23T12:05:48.223Z') },
+        { _id: '12345678901234567890dbcp', value : 1, timestamp : new Date('2012-03-23T13:33:48.223Z') },
+        { _id: '12345678901234567890dbcq', value : 1, timestamp : new Date('2012-03-23T13:43:48.223Z') },
+        { _id: '12345678901234567890dbcr', value : 1, timestamp : new Date('2012-03-23T13:50:48.223Z') },
+        { _id: '12345678901234567890dbcs', value : 0, timestamp : new Date('2012-03-23T13:51:48.223Z') } ]
+}, {
 
     users: [
         {_id: '111111111111111111111100', fbUserId: '111111', fbInformation: {name: 'Pyry Kröger'}}, // No points
