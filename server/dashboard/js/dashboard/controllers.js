9f83375a99beefa8d0074d90a0fff6d75bf19d3b
Lauri Orkoneva <lauri.orkoneva@gmail.com>
2012-09-21 17:43:14 +0300

    Graphdata from backend

--- server/dashboard/js/dashboard/controllers.js
+++ server/dashboard/js/dashboard/controllers.js
@@ -1,59 +1,106 @@
 (function(global) {
-
     var App;
     global.App = App = Spine.Controller.sub({
         init: function() {
-            // Set templates
-            TargetItem.extend({template: Handlebars.compile($("#targetitem-tmpl").html())}),
-
-            // Create instances
-            new Targets({el: $('#targets')});
-            global.Target.fetch();
+            Spine.Model.host = "http://86.50.145.125/";
+            SidebarQuestionItem.extend({template: Handlebars.compile($("#sidebar-tmpl").html())});
+            Chart.extend({template: Handlebars.compile($("#chart-tmpl").html())});
+            new SidebarQuestions({el: $('#sidebar')});
+            global.Question.fetch();
+            new Chart({el: $('#chart')});
         }
     });
 
-    var TargetItem;
-    global.TargetItem = TargetItem = Spine.Controller.sub({
-        tag: 'li',
-
-        // Bind events to the record
+    var Chart;
+    global.Chart = Chart = Spine.Controller.sub({
         init: function() {
-            if ( !this.item ) throw "target required";
+            console.log(Question.all());
+            Dashboard.Question.bind("refresh", this.proxy(this.addOne));
+
+            //if ( !this.item ) throw "question required";
+            //this.item.bind("update", this.proxy(this.render));
+            //this.item.bind("destroy", this.proxy(this.removeEl));
+        },
+        addOne: function() {
+            console.log("Chart addOne");
+            console.log(Question.all());
+            var question = Question.all()[0];
+            var seriesData = [ [],[] ];
+            for (var i = 0; i < question.results.timeDistribution.length; i++) {
+                seriesData[0].push({x: i, y: question.results.timeDistribution[i].pos_sum});
+                seriesData[1].push({x: i, y: question.results.timeDistribution[i].neg_sum});
+            }
+
+
+            /*
+
+             seriesData[0].push({x: time, y: pos}); // pos
+             seriesData[1].push({x: time, y: neg}); // neg
+             */
+            console.log(seriesData);
+
+            this.drawGraph(seriesData);
+
+        },
+        render: function(item){
+            if (item) this.item = item;
+            this.html(Chart.template(this.item));
+            return this;
+        },
+        removeEl: function() {
+            this.el.remove();
+        },
+        drawGraph: function(seriesData) {
+            var graph;
+            graph = new Rickshaw.Graph( {
+                element: document.getElementById("chart"),
+                width: 470,
+                height: 300,
+                renderer: 'bar',
+                series: [
+                    {
+                        color: "#c05020",
+                        data: seriesData[0]
+                    }, {
+                        color: "#30c020",
+                        data: seriesData[1]
+                    }
+                ]
+            } );
+            graph.render();
+        }
+    });
+
+    var SidebarQuestionItem;
+    global.SidebarQuestionItem = SidebarQuestionItem = Spine.Controller.sub({
+        init: function() {
+            if ( !this.item ) throw "question required";
             this.item.bind("update", this.proxy(this.render));
             this.item.bind("destroy", this.proxy(this.removeEl));
         },
-
         render: function(item){
             if (item) this.item = item;
-
-            this.html(TargetItem.template(this.item));
-            console.log(this.item);
+            this.html(SidebarQuestionItem.template(this.item));
             return this;
         },
-
         removeEl: function() {
             this.el.remove();
         }
     });
 
-    var Targets;
-    global.Targets = Targets = Spine.Controller.sub({
-        el: $('#targets'),
-
+    var SidebarQuestions;
+    global.SidebarQuestions = SidebarQuestions = Spine.Controller.sub({
         init: function(){
-            Dashboard.Target.bind("refresh", this.proxy(this.addAll));
-            Dashboard.Target.bind("create",  this.proxy(this.addOne));
+            Dashboard.Question.bind("refresh", this.proxy(this.addAll));
+            Dashboard.Question.bind("create",  this.proxy(this.addOne));
         },
-
         addOne: function(item){
-            debugger;
-            var target = new TargetItem({item: item});
-            this.append(target.render());
+            console.log("SidebarQuestions addone");
+            var question = new SidebarQuestionItem({item: item});
+            this.append(question.render());
         },
-
         addAll: function(){
-            Target.each(this.proxy(this.addOne));
+            Question.each(this.proxy(this.addOne));
         }
     });
-
 })(Dashboard);
--- server/dashboard/js/dashboard/models.js
+++ server/dashboard/js/dashboard/models.js
@@ -1,16 +1,17 @@
 (function(global) {
 
-    global.Target = Target = Spine.Model.sub();
-    Target.configure('Target', 'name', 'question', 'results');
+    global.Question = Question = Spine.Model.sub();
+    Question.configure('Question', 'name', 'id', 'results');
 
-    Target.extend(Spine.Model.Ajax);
+    Question.extend(Spine.Model.Ajax);
 
-    Target.extend({
+    Question.extend({
+        url: "http://86.50.143.98/questions_dashboard",
         fromJSON: function(objects) {
             // TODO
             // Change the API according to Spine.Ajax and remove this hackish method
-            var targets = (typeof objects === 'string' ? JSON.parse(objects) : objects).targets;
-            return Spine.Model.fromJSON.apply(this, [targets]);
+            var questions = (typeof objects === 'string' ? JSON.parse(objects) : objects).questions;
+            return Spine.Model.fromJSON.apply(this, [questions]);
         }
     });
 
