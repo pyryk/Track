265ab7860d0d3c53d41bd1e27ceb68a0e69789b1
Lauri Orkoneva <lauri.orkoneva@gmail.com>
2012-10-04 13:21:21 +0300

    Starting to add Customers also to dashboard

--- server/dashboard/js/dashboard/controllers.js
+++ server/dashboard/js/dashboard/controllers.js
@@ -3,12 +3,16 @@
     global.App = App = Spine.Controller.sub({
         init: function() {
             Spine.Model.host = "http://86.50.145.125/";
-            SidebarQuestionItem.extend({template: Handlebars.compile($("#sidebar-tmpl").html())});
+            //setting templates
+            Sidebar.extend({template: Handlebars.compile($("#sidebar-tmpl").html())});
             Title.extend({template: Handlebars.compile($("#title-tmpl").html())});
+
+            new SidebarCustomer({el: $('#sidebar')});
             new SidebarQuestions({el: $('#sidebar')});
             new Chart({el: $('#chart')});
             new Title({el: $('#title')});
             global.Question.fetch();
+            global.Customer.fetch();
         }
     });
 
@@ -18,7 +22,6 @@
             Dashboard.Question.bind("refresh", this.proxy(this.setTitle));
         },
         setTitle: function() {
-            console.log(Question.all()[0].name);
             this.html(Title.template(Question.all()[0]));
         }
     });
@@ -37,15 +40,12 @@
                 seriesData[0].push({x: date, y: question.results.timeDistribution[i].pos_sum});
                 seriesData[1].push({x: date, y: question.results.timeDistribution[i].neg_sum});
             }
-
             alltimeData[0].push({x: 0, y: 0});
             alltimeData[1].push({x: 0, y: 0});
             alltimeData[0].push({x: 1, y: question.results.alltime.pos});
             alltimeData[1].push({x: 1, y: question.results.alltime.neg});
             alltimeData[0].push({x: 2, y: 0});
             alltimeData[1].push({x: 2, y: 0});
-
-
             this.drawGraph(seriesData, alltimeData);
         },
         drawGraph: function(seriesData, alltimeData) {
@@ -81,16 +81,21 @@
         }
     });
 
-    var SidebarQuestionItem;
-    global.SidebarQuestionItem = SidebarQuestionItem = Spine.Controller.sub({
+
+
+
+    var Sidebar;
+    global.Sidebar = Sidebar = Spine.Controller.sub({
         init: function() {
             if ( !this.item ) throw "question required";
+            console.log("Luodaan ja lisätään yksi uusi Sidebar: " + this.item.name);
             this.item.bind("update", this.proxy(this.render));
             this.item.bind("destroy", this.proxy(this.removeEl));
         },
         render: function(item){
             if (item) this.item = item;
-            this.html(SidebarQuestionItem.template(this.item));
+            console.log("Renderöidään SidebarQuestionItem: " + this.item.name);
+            this.html(Sidebar.template(this.item));
             return this;
         },
         removeEl: function() {
@@ -98,14 +103,31 @@
         }
     });
 
+    var SidebarCustomer;
+    global.SidebarCustomer = SidebarCustomer = Spine.Controller.sub({
+        init: function(){
+           console.log("Luodaan SidebarCustomer -oliota");
+           Dashboard.Customer.bind("refresh", this.proxy(this.addAll));
+           Dashboard.Customer.bind("create",  this.proxy(this.addOne));
+        },
+        addOne: function(item){
+            var customer = new Sidebar({item: item});
+            this.append(customer.render());
+        },
+        addAll: function(){
+            Customer.each(this.proxy(this.addOne));
+        }
+    });
+
     var SidebarQuestions;
     global.SidebarQuestions = SidebarQuestions = Spine.Controller.sub({
         init: function(){
+            console.log("Luodaan SidebarQuestion -olio");
             Dashboard.Question.bind("refresh", this.proxy(this.addAll));
             Dashboard.Question.bind("create",  this.proxy(this.addOne));
         },
         addOne: function(item){
-            var question = new SidebarQuestionItem({item: item});
+            var question = new Sidebar({item: item});
             this.append(question.render());
         },
         addAll: function(){
--- server/dashboard/js/dashboard/models.js
+++ server/dashboard/js/dashboard/models.js
@@ -1,12 +1,12 @@
 (function(global) {
 
+    var ip = "http://86.50.143.102/";
+
     global.Question = Question = Spine.Model.sub();
     Question.configure('Question', 'name', 'id', 'results');
-
     Question.extend(Spine.Model.Ajax);
-
     Question.extend({
-        url: "http://86.50.143.98/questions_dashboard",
+        url: ip + "questions_dashboard",
         fromJSON: function(objects) {
             // TODO
             // Change the API according to Spine.Ajax and remove this hackish method
@@ -14,5 +14,18 @@
             return Spine.Model.fromJSON.apply(this, [questions]);
         }
     });
+
+    global.Customer = Customer = Spine.Model.sub();
+    Customer.configure('Customer', 'name', 'id', 'targets');
+    Customer.extend(Spine.Model.Ajax);
+    Customer.extend({
+        url: ip,
+        fromJSON: function(objects) {
+            // TODO
+            // Change the API according to Spine.Ajax and remove this hackish method
+            var customers = (typeof objects === 'string' ? JSON.parse(objects) : objects).customers;
+            return Spine.Model.fromJSON.apply(this, [customers]);
+        }
+    })
 
 })(Dashboard);
